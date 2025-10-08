dd1983020de6b63e6cde5f7957448ba0fa05799d
박동현 <jinee9523@gmail.com>
2025-10-08 10:23:50 +0900

    Feat: initial commit

---
+++ src/test/java/site/donghyeon/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package site.donghyeon.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
