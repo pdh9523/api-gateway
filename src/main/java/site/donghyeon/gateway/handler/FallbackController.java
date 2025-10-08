0a26502de5dfe9b745e3c1028b49049c8972c991
박동현 <jinee9523@gmail.com>
2025-10-09 01:53:54 +0900

    comment: sout 로그 slf4j 활용으로 변경

--- src/main/java/site/donghyeon/gateway/handler/FallbackController.java
+++ src/main/java/site/donghyeon/gateway/handler/FallbackController.java
@@ -1,5 +1,6 @@
 package site.donghyeon.gateway.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -7,10 +8,11 @@ import reactor.core.publisher.Mono;
 import java.util.Map;
 
 @RestController
+@Slf4j
 public class FallbackController {
     @GetMapping("/__fallback")
     public Mono<Map<String, String>> fallback() {
-        System.out.println("check fallback");
+        log.error("Circuit breaker fallback triggered");
         return Mono.just(Map.of("status", "fallback", "message", "Service temporarily unavailable"));
     }
 }
