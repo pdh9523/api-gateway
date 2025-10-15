4adb80d9e16f9853e3fd318803f2a502a8d6ce4f
박동현 <jinee9523@gmail.com>
2025-10-15 16:28:01 +0900

    refactor: 허용/인증 필터 분리

--- src/main/java/site/donghyeon/gateway/config/SecurityConfig.java
+++ src/main/java/site/donghyeon/gateway/config/SecurityConfig.java
@@ -2,24 +2,35 @@ package site.donghyeon.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
+    @Order(1)
+    public SecurityWebFilterChain publicFilterChain(ServerHttpSecurity http) {
+        return http
+                .securityMatcher(ServerWebExchangeMatchers.pathMatchers("/test/**", "/dead/**"))
+                .authorizeExchange(exchange -> exchange.anyExchange().permitAll())
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .build();
+    }
+
+    @Bean
+    @Order(2)
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
                 .authorizeExchange(exchange -> exchange
-                        .pathMatchers("/test/**").permitAll()
-                        // 권한 기반 라우팅 예시
                         .pathMatchers("/book/**").hasRole("ADMIN")
                         .pathMatchers("/search/**").hasAnyRole("USER", "ADMIN")
                         .anyExchange().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt
-                        .jwtAuthenticationConverter(jwtRoleConverter())  // ← 커스텀 Converter 등록
+                        .jwtAuthenticationConverter(jwtRoleConverter())
                 ))
                 .build();
     }
